22bd2f881116515ab2925c30a8d4818b2d820c6a
unknown <izzat@gmail.com>
2023-04-11 14:59:04 +0500

    added links to Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import { catalogLink, footerLinks } from "./data";
 
 const Footer = () => {
     return (
@@ -11,94 +12,29 @@ const Footer = () => {
                             Информация
                         </h2>
                         <nav className='footer-content__item-nav'>
-                            <a href='#' className='footer-content__links'>
-                                О нас
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Доставка
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Политика безопасности
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Условия соглашения
-                            </a>
+                            {footerLinks.map((el) => (
+                                <a
+                                    href='/'
+                                    className='footer-content__links'
+                                    key={el.id}
+                                >
+                                    {el.link}
+                                </a>
+                            ))}
                         </nav>
                     </div>
                     <div className='footer-content__item'>
-                        <h2 className='footer-content__item-title'>
-                            Информация
-                        </h2>
+                        <h2 className='footer-content__item-title'>Каталог</h2>
                         <nav className='footer-content__item-nav'>
-                            <a href='#' className='footer-content__links'>
-                                О нас
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Доставка
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Политика безопасности
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Условия соглашения
-                            </a>
-                        </nav>
-                    </div>
-                    <div className='footer-content__item'>
-                        <h2 className='footer-content__item-title'>
-                            Информация
-                        </h2>
-                        <nav className='footer-content__item-nav'>
-                            <a href='#' className='footer-content__links'>
-                                О нас
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Доставка
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Политика безопасности
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Условия соглашения
-                            </a>
-                        </nav>
-                    </div>
-                    <div className='footer-content__item'>
-                        <h2 className='footer-content__item-title'>
-                            Информация
-                        </h2>
-                        <nav className='footer-content__item-nav'>
-                            <a href='#' className='footer-content__links'>
-                                О нас
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Доставка
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Политика безопасности
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Условия соглашения
-                            </a>
-                        </nav>
-                    </div>
-                    <div className='footer-content__item'>
-                        <h2 className='footer-content__item-title'>
-                            Информация
-                        </h2>
-                        <nav className='footer-content__item-nav'>
-                            <a href='#' className='footer-content__links'>
-                                О нас
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Доставка
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Политика безопасности
-                            </a>
-                            <a href='#' className='footer-content__links'>
-                                Условия соглашения
-                            </a>
+                            {catalogLink.map((el) => (
+                                <a
+                                    href='/'
+                                    className='footer-content__links'
+                                    key={el.id}
+                                >
+                                    {el.text}
+                                </a>
+                            ))}
                         </nav>
                     </div>
                 </div>
