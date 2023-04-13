f6400c0d7fc80736f32d5d19114451ebc5d88707
unknown <izzat@gmail.com>
2023-04-13 15:59:15 +0500

    added Counter component

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,17 +6,19 @@ import Header from "./components/Header";
 import Order from "./components/Order";
 import Book from "./components/Book";
 import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 const Home = () => {
     return (
         <>
-            <Header />
+            {/* <Header />
             <Category />
             <Discount />
             <Order />
             <Data />
             <Book />
-            <Footer />
+            <Footer /> */}
+            <Counter />
         </>
     );
 };
